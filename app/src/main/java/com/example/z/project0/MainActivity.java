1ff3dd73f6914cf11db922c4b2198d15157feea2
sz2git <zs2041@gmail.com>
2016-11-17 20:01:42 +0800

    First Run

--- app/src/main/java/com/example/z/project0/MainActivity.java
+++ app/src/main/java/com/example/z/project0/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.z.project0;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessageButton1(View view) {
+        android.content.Context context = getApplicationContext();
+        CharSequence text = "Hello toast2!";
+        int duration = android.widget.Toast.LENGTH_SHORT;
+
+        android.widget.Toast toast = android.widget.Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
